043325a1f9b0ff158b5a0ad1265a1dc742d6dc64
alanwhelan1978 <alanwhelan1978@gmail.com>
2021-11-22 14:39:16 +0000

    Add event listeners fopr document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
+// Wait for the DOM to finish loading before running the game
+
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons){
+       button.addEventListener("click", function() {
+        if (this.getAttribute("data-type") === "submit") {
+            alert('You rolled the dice')
+        } 
+       }) 
+    }
+})
+
 function runGame() {
 
 }
